85014443c4e6b4082e4951c74762ab5fbf89c888
Thomas Fabritius <tfabritius@gmail.com>
2024-01-03 16:31:54 +0100

    Add readme

---
+++ README.md
@@ -0,0 +1,9 @@
+# Covermaker
+
+Little image manipulation tool to resize images to a different aspect ratio by adding some fuzzy background.
+
+## Usage
+
+- Install [deno](https://docs.deno.com/runtime/manual/getting_started/installation)
+- Run `deno run -A main.ts -h` to show help
+- Example: `deno run -A main.ts -r 75/50 image1.png image2.jpg`
--- main.ts
+++ main.ts
@@ -145,16 +145,16 @@ if (args.help) {
 
 function showHelp() {
   console.log(`
-Usage: deno run --allow-read script.ts [OPTIONS] [FILES]
+Usage: deno run -A main.ts [OPTIONS] [FILES]
 
 Options:
   -b, --blur <value>   Set the blur factor (default: 1.0)
-  -r, --ratio <value>  Set the target aspect ratio value (default: 1.5)
-  --rotate on|off|auto Rotate the image by 90°
+  -r, --ratio <value>  Set the target aspect ratio value (default: 3/2 or 1.5)
+  --rotate on|off|auto Rotate the image by 90° (default: auto)
   -h, --help           Show this help message and exit
 
 Example:
-  deno run --allow-read script.ts -r 75/50 file1.txt file2.txt
+  deno run -A main.ts -r 75/50 image1.png image2.jpg
   `);
 }
 
