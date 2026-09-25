635310eeb4172fe59f7e11b89baba5f4ead70525
Thomas Fabritius <tfabritius@gmail.com>
2026-09-25 22:20:16 +0200

    Exclude e2e tests when running unit tests

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,5 +3,6 @@ import { defineVitestConfig } from '@nuxt/test-utils/config'
 export default defineVitestConfig({
   test: {
     environment: 'node',
+    exclude: ['tests/e2e/**', 'node_modules/**'],
   },
 })
